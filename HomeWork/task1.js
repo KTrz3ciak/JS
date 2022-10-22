32b983b3bbd70cf4ec5834c7dda882fc06e468ba
Konrad Trzeciak <trzeciak.konrad96@gmail.com>
2022-10-22 14:33:28 +0200

    update

--- HomeWork/task1.js
+++ HomeWork/task1.js
@@ -6,11 +6,11 @@ let sentence = "Increase your productivity with AI-powered writing assistant";
 function numberOfWords(word) { 
   return word.split(" ").length; 
 }
-console.log(numberOfWords(sentence));
+console.log("Count words for sentence is " + numberOfWords(sentence));
 // b.	count how many letter “A”  is in it
 let letterOfSentences = sentence.split("a");
 //insert your string here
-console.log(letterOfSentences.length - 1);
+console.log("Count letter a on sentence is " + (letterOfSentences.length - 1));
 
 // c.	display longest word
 const longestWord = (sentence) => {

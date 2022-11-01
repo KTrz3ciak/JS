43877f005ba0ab9c30f4cd88e62773b7eff8902c
Konrad Trzeciak <trzeciak.konrad96@gmail.com>
2022-11-01 19:08:01 +0100

    1.f is done now

--- HomeWork/homework2.js
+++ HomeWork/homework2.js
@@ -23,9 +23,13 @@
 
 
 //1.b
-let array = [1, 6, 23, 8, 4, 8, 3, 7];
-console.log("Length of array is: " + array.length);
-console.log("Start array: " + array);
+let myArray = [1, 6, 23, 8, 4, 8, 3, 7];
+let rArr = Math.floor(Math.random() * 8);
+let ranArray = Math.floor(Math.random() * myArray.length);
+console.log("Random array: " + rArr);
+console.log("Random index from array is: " + ranArray);
+console.log("Length of array is: " + myArray.length);
+console.log("Start array: " + myArray);
 // console.log(array.length);
 function sumArray(inputArray) {
     let sum = 0;
@@ -34,7 +38,7 @@ function sumArray(inputArray) {
     }
     return sum;
 }
-console.log(sumArray(array));
+console.log(sumArray(myArray));
 
 // 1.c
 let sumFirstLast = 0;
@@ -45,43 +49,46 @@ function sumFirstLastArray(inputArray) {
     }
     return sum;
 }
-console.log(sumFirstLastArray(array));
+console.log(sumFirstLastArray(myArray));
 
 // 1.d
-function reverseArray(inputArray) {
+function reverseArray() {
     let output = [];
     // for (var i = 0; i < inputArray.length; i++) {
     //     output.push(inputArray.pop());;       // only 4 last number will  be reversed bcs i === 4 in this loop
     // }
     // return output
-     while (array.length) {     // length of array
-        output.push(array.pop()); // method push / pop | push method -> first element == last element &&
+     while (myArray.length) {     // length of array
+        output.push(myArray.pop()); // method push / pop | push method -> first element == last element &&
     }                             // pop method -> takes the last element from the array and returns it 
   return output;
 }
-console.log("Reverse array: " + reverseArray(array));
+console.log("Reverse array: " + reverseArray(myArray));
 // 1.e
 // e.	Create a function that takes two parameters - array of numbers, and number of attempts. Choose random numbers from the array based on the number of attempts and return the lowest among them.
- function arrayOfNumbersAndAttemps(array, attemps)
-{
-    let lowestScore = [];
-     let randomArrayItems = Math.floor(Math.random() * array.length);
-     let random = array[randomArrayItems];
-    // for (let i = 0; i < array.length; i++) {
-    //     if (array[i] > randomArrayItems) 
-    //     {
-    //         output = lowestScore.push(i);
-    //     }
-        
-    //     return output;
-    // }
-}
-console.log(arrayOfNumbersAndAttemps(array));
+//  function arrayOfNumbersAndAttemps(array, attemps)
+// {
+//      let lowestScore = 0;
+//      let output;
+//     //  let randomArrayItems = Math.floor(Math.random() * array.length);
+//     //  let random = array[randomArrayItems];
+//     attemps = myArray.length;
+//      for (let i = 0; i < myArray.length; i++) {
+//          output = "Czy tutaj coś sie dzieje ? " + i + myArray[i];
+//          console.log(output);
+//         if (array[i] > lowestScore) 
+//         {
+//             lowestScore = myArray[i]
+//         } 
+//      }
+//     return lowestScore;
+// }
+// console.log(arrayOfNumbersAndAttemps(myArray));
 // 1.f
-function randomArray(randomA) {
-    // let randomA = Math.floor(raMath.random() * array.length);
-    // return array[Math.floor(Math.random() * array.length)];
-    let randomArray = Math.floor(Math.random() * randomA.length)
-    return randomArray[randomA];
+let myArray2 = [1, 6, 23, 8, 4, 8, 3, 7];
+function getRandomArray() {
+    let randomArray = Math.floor(Math.random() * myArray2.length);
+    return myArray2[randomArray];
 }
-console.log(randomArray(array));
+console.log("Random number of my array is: " + getRandomArray(myArray2));
+

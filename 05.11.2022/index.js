1c68e74f009ed550a8bcfe588f0fb2157902f3ae
Konrad Trzeciak <trzeciak.konrad96@gmail.com>
2022-11-05 15:03:54 +0100

    Programs from  Lessons

--- 05.11.2022/index.js
+++ 05.11.2022/index.js
@@ -12,7 +12,7 @@
 // }
 
 const numbers = [1, 5, 3, 6, 8, 2, 10, 10, 283, 4, 7];
-const characters = ["a", "b", "c", "d"];
+const character = ["a", "b", "c", "d"];
 numbers.sort((a, b) => a - b)
 //{
     // if (a > b) {
@@ -64,4 +64,65 @@ const result5 = numbers.map(number => number * 2)
 
 console.log(result5);
 
+const numbers6 = [4, 1, 3, 2, 12, 3, 29, 4, 6, 34, 16, 28];
+const result6 = numbers6.map((number) => number * 2).filter((number) => number < 40).sort((a, b) => b - a);
+console.log(result6);
 
+
+const episodes = require("./episodes.json");
+const characters = require("./characters.json");
+const locations = require("./locations.json");
+
+function allDeadPeople() {
+    const result = characters.reduce((sum, currentElement) => {
+        if (currentElement.status === 'Dead') {
+            return sum + 1;
+        } else {
+            return sum;
+        }
+    }, 0)
+    return result;
+}
+console.log(allDeadPeople())
+
+function mapAllCharactersLivingOnEarth() {
+    const result = characters.filter((character) => {
+        // (character.origin.name.includes('Earth')
+        if (character.origin.name.includes('Earth')) {
+            return true;
+        }
+        return false;
+    }).sort((a, b) => {
+        if (a.name > b.name){
+            return 1;
+        }else if (a.name < b.name) {
+            return -1;
+        }else if (a.name === b.name){
+            return 0;
+        }
+    })
+    return result;
+}
+// console.log(mapAllCharactersLivingOnEarth());
+// console.log('a');
+
+// 15-04-2018
+// const evalDate = new Date(2018, 3, 15);   // 3 bo to index miesiaca 3 
+const evalDate = new Date('2018-04-15'); 
+const evalDateMS = evalDate.getTime();
+
+console.log(evalDateMS);
+function mapCharactersByDate() {
+    const result = characters.filter(currChar => {
+        const creationDate = new Date(currChar.created);
+        const creationDateMS = creationDate.getTime();
+        if (creationDate < evalDateMS)
+        {
+            return true;
+        } 
+        return false;
+    })
+    return result;
+}
+console.log(mapCharactersByDate());
+const number = 1;

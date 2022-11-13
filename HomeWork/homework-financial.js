64454b4221fe726ca1010cff6e8bbe3d3196eac3
Konrad Trzeciak <trzeciak.konrad96@gmail.com>
2022-11-13 16:11:19 +0100

    Something new but not correct probably

--- HomeWork/homework-financial.js
+++ HomeWork/homework-financial.js
@@ -13,8 +13,15 @@ console.log('Financial data: ' + getFiancialObject());
 function getFiancialObject() {
   const financialObject = financialData.reduce((sum, currentElement) => {
     const costNumber = Math.round(currentElement.cost * 10) / 10;
+    const nameCompany = currentElement.detailsOfPayent.company;
     if (currentElement.cost > 0 && currentElement.detailsOfPayent.date.includes('2014')) {
       // console.log("There is cost: " + costNumber);
+      return sum + costNumber;
+    } else if (currentElement.cost > 0 && currentElement.detailsOfPayent.date.includes('2014'))
+    {
+      return sum + costNumber;
+    } else if (currentElement.cost > 0 && currentElement.detailsOfPayent.date.includes('2014')) 
+    {
       return sum + costNumber;
     }
     return sum;
@@ -23,3 +30,50 @@ function getFiancialObject() {
 
   return financialObject;
 }
+// function earingsPerCompany() {
+//   const earings = financialData.filter((company) =>
+//   {
+//     if (company.detailsOfPayent.company === "ECSTASIA")
+//     {
+//       console.log("There is ECSTASIA company. ");
+//       return true;
+//     } else if(company.detailsOfPayent.company === "MANGELICA")
+//     {
+//       console.log("There is MANGELICA company. ");
+//       return true
+//     } else if(company.detailsOfPayent.company === "CODAX")
+//     {
+//       console.log("There is CODAX company. ");
+//       return true;
+//     }
+//     return false;
+//   })
+//   return earings;
+// }
+// earingsPerCompany();
+
+function earingsPerCompany() {
+  const earings = financialData.reduce((sum, currentElement) => {
+    const nameCompany = currentElement.detailsOfPayent.company;
+    const costNumber = Number(currentElement.cost);
+    if (currentElement.cost > 0 && currentElement.detailsOfPayent.date.includes('2014')) {
+      // console.log("There is cost: " + costNumber);
+      if (currentElement.detailsOfPayent.company.includes("ECSTASIA")) {
+        console.log("ECSTASIA earn: " + sum);
+        return sum + costNumber;
+      } else if (currentElement.detailsOfPayent.company.includes("MANGELICA")) {
+        console.log("MANGELICA here: " + sum);
+        return sum + costNumber;
+      } else if (currentElement.detailsOfPayent.company.includes("CODAX")) {
+        console.log("CODAX here: " + sum);
+        return sum + costNumber;
+      } else
+      {
+        return 0;
+      };
+    }
+    return sum;
+  },0);
+  return earings;
+}
+console.log(earingsPerCompany());

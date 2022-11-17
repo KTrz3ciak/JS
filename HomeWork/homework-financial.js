226434aeaa32bad389f7afe5ccdc4bb3e3d7ee26
Konrad Trzeciak <trzeciak.konrad96@gmail.com>
2022-11-17 17:45:28 +0100

    Update

--- HomeWork/homework-financial.js
+++ HomeWork/homework-financial.js
@@ -13,32 +13,25 @@ console.log('Financial data: ' + getFiancialObject());
 function getFiancialObject() {
   const financialObject = {
     moneySpentAt2014: getSpendingsIn2014(),
-    earningPerCompany(nameCompany, moneySpent) {
-      return financialData[nameCompany] += moneySpent;
-    },
-    getSpendingsPerTransactionType(type, moneySpent)
-    {
-      return financialData[type] += moneySpent;
-    }
+    earningPerCompany: getEaringsPerCompany()
+    // getSpendingsPerTransactionType(type, moneySpent)
+    // {
+    //   return financialData[type] += parseInt(moneySpent);
+    // }
     
   };
   // TODO (create functions for calculations below)
   return financialObject;
 }
+
+
 console.log(getFiancialObject());
 
 function getSpendingsIn2014() {
   const moneySpent = financialData.reduce((sum, currentElement) => {
     const costNumber = Math.round(currentElement.cost * 10) / 10;
-    const nameCompany = currentElement.detailsOfPayent.company;
     if (currentElement.cost > 0 && currentElement.detailsOfPayent.date.includes('2014')) {
       // console.log("There is cost: " + costNumber);
-      return sum + costNumber;
-    } else if (currentElement.cost > 0 && currentElement.detailsOfPayent.date.includes('2014'))
-    {
-      return sum + costNumber;
-    } else if (currentElement.cost > 0 && currentElement.detailsOfPayent.date.includes('2014')) 
-    {
       return sum + costNumber;
     }
     return sum;
@@ -49,28 +42,22 @@ function getSpendingsIn2014() {
 }
 console.log(getSpendingsIn2014());
 
-
-// let sum = 0;
-// const numbers = [65, 44, 12, 4];
-// numbers.forEach(myFunction);
-
-// function myFunction(item) {
-//   sum += item;
-
-// function getEaringsPerCompany(nameCompany, currentCost) {
-//     const earnings = financialData.reduce((nameCompany, currentElement) =>
-//     {
-//       const currentCompany = currentElement.detailsOfPayent.company;
-//       const currentCost = currentElement.cost;
-//     if (currentCost > 0 && currentCompany[nameCompany])
-//     {
-//        return currentCompany[nameCompany] += currentCost;
-//     }
-//     return nameCompany;
-//   },0)
-//   return earnings;
-// }
-// console.log(getEaringsPerCompany());
+function getEaringsPerCompany() {
+  return financialData.reduce((nameCompany, currentElement) => {
+    const currentCompany = currentElement.detailsOfPayent.company;
+    const currentCost = currentElement.cost;
+    // console.log(currentCompany);
+    financialData.forEach((elements) => {
+          return nameCompany[currentCompany] += +currentCost;
+        });
+        if (nameCompany[currentCompany] === undefined)
+        {
+        nameCompany[currentCompany] = 0;
+        }
+        return nameCompany;
+      }, {})     
+    }
+    console.log(getEaringsPerCompany());
 
 
 // function getEaringsPerCompanyMANGELICA() {
